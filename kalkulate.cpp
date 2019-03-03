5056cea843aa1f8d96fda13b7d44b8d3cfa382fc
idfhwbrfq3 <48187264+idfhwbrfq3@users.noreply.github.com>
2019-03-03 21:07:34 +0100

    Update kalkulate.cpp

--- kalkulate.cpp
+++ kalkulate.cpp
@@ -7,3 +7,31 @@ float a, b, c;
 char d;
 int p;
 int main()
+  
+  {
+//Kyrylo Kaibedov 21:15 / 3.03.2019
+    while (p != 2)
+    {
+        printf("1 = calculator \n 2 - exit \n");
+        scanf("%d", &p);
+        switch (p)
+        {
+        case 1:
+        {
+            float c;
+            printf("First number ");
+            scanf("%d", &a);
+            printf("Action with numbers ");
+            scanf("%s", &d);
+            printf("Second number ");
+            scanf("%d", &b);
+            if (d == '+')
+                c = a + b;
+            if (d == '-')
+                c = a - b;
+            if (d == '*')
+                c = a * b;
+            if (d == '/')
+                c = a / b;
+            system("cls");
+        }
